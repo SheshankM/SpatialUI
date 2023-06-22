46060dae276d16a9ff4a0e2d7d3a23e3c4f3e341
SheshankM <108621304+SheshankM@users.noreply.github.com>
2023-06-22 12:13:30 +0530

    add twitter mainbar tweets

--- src/components/TweetBox.tsx
+++ src/components/TweetBox.tsx
@@ -1,12 +1,32 @@
 import "../components/styles/tweetbox.css"
+import profile from "../assets/background1.jpg"
+import {BsChat,BsThreeDots} from "react-icons/bs"
+import {LuShare} from "react-icons/lu"
+
+import {AiOutlineHeart,AiOutlineRetweet} from "react-icons/ai"
 const TweetBox = () => {
   return (
     <div className="tweet-box">
       <div className="profile-pic">
-        <img src="" alt="" />
+        <img src={profile} alt=""/>
       </div>
-      <div className="tweet-body"></div>
-      <div className="tweet-dots"></div>
+      <div className="tweet-body">
+        <div className="username">
+          <span>SheshankM8</span><span className="unq">@shank__it</span>
+        </div>
+        <div className="tweet" >
+          Another day another ui kit
+        </div>
+        <div className="metrics">
+          <div><BsChat/><span>16</span></div>
+          <div><AiOutlineRetweet/></div>
+          <div><AiOutlineHeart/></div>
+          <div><LuShare/></div>
+        </div>
+      </div>
+      <div className="tweet-dots">
+      <BsThreeDots/>
+      </div>
     </div>
   )
 }
